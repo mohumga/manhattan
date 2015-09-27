007bec596c51d50b9c1fe2a4392905bf052a3b4c
Keunuk Lee <nabesin82@naver.com>
2015-09-27 12:53:01 +0900

    Initial commit.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Top t = new Top();
+            ThreadStart ts = new ThreadStart(t.SayHello);
+            Thread thread = new Thread(ts);
+            thread.Start();
+            Console.Write("Thread + " + Thread.CurrentThread.GetHashCode() + "메인 종료\n");
+        }
+    }
+
+    public class Top
+    {
+        private int limit = 0;
+
+        public void SayHello()
+        {
+            while (limit < 10)
+            {
+                Thread th = Thread.CurrentThread;
+                Console.WriteLine("Thread " + th.GetHashCode() + ":" + limit++);
+            }
+        }
+    }
+}
